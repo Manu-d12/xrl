cab7febc5db020f50b530a512a93bd696bb57cae
Kuntal Sarkar <kuntal.sarkar383@gmail.com>
2022-08-29 13:18:20 +0530

    Added Custom select component

--- xrl-core/main/default/lwc/exrlTab/exrlTab.js
+++ xrl-core/main/default/lwc/exrlTab/exrlTab.js
@@ -4,31 +4,26 @@ import { libs } from 'c/libs';
 export default class ExrlTab extends LightningElement {
     @track config = {};
     @track objList = [];
-    @track extApiName = '';
+    @track extApiName = 'Account:Account:';
     selectedApiName = '';
     connectedCallback(){
-        // this.config = libs.getGlobalVar('Case');
         libs.remoteAction(this, 'objectList', {callback: this.getAllObjects.bind(this) });
     }
 
     getAllObjects(cmd,data){
         data[cmd].sort().forEach((el)=>{
-            this.objList.push({label:el,value:el});
+            this.objList.push({'label':el.toString(),'value':el.toString()});
         });
     }
-    selectObject(event){
-        let selectedObj = event.detail.slice(-1)[0];
-        if(selectedObj !== undefined){
-            this.selectedApiName = selectedObj + ':' + selectedObj + ':';
-        }else{
-            this.selectedApiName = '';
-            this.extApiName = this.selectedApiName;
-        }
-    }
     handleOpenGrid(event){
-        // this.template.querySelector('c-ext-rel-list').updateGridView(this.extApiName);
-        this.extApiName = '';
-        this.extApiName = this.selectedApiName;
-        this.template.querySelector('c-ext-rel-list').updateGridView(this.extApiName);
+        this.template.querySelector('c-ext-rel-list').updateGridView(this.selectedApiName);
     }
+
+    handleSelect(event) {
+        let selectedObj = event.detail.payload.value;
+        if(selectedObj !== null){
+            this.selectedApiName = selectedObj + ':' + selectedObj + ':';
+        }
+        
+       }
 }
