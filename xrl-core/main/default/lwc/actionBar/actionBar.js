ca8cd06acf22c21e69ae9cbe8731d989d564d341
Kuntal Sarkar <kuntal.sarkar383@gmail.com>
2022-09-14 11:47:39 +0530

    HYPER-36 || Main logic implemented

--- xrl-core/main/default/lwc/actionBar/actionBar.js
+++ xrl-core/main/default/lwc/actionBar/actionBar.js
@@ -5,4 +5,14 @@ export default class ActionBar extends LightningElement {
     connectedCallback(){
         console.log(JSON.parse(JSON.stringify(this.actionscfg)));
     }
+    handleEventClick(event){
+        let actionId = (event.currentTarget.id).slice(0, -4);
+        let actionDetails;
+        this.actionscfg.forEach((el)=>{
+            if(el.id === actionId){
+                actionDetails = el;
+            }
+        });
+        actionDetails.callback();
+    }
 }
