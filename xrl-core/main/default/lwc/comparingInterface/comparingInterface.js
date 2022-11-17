1980243f972a0e5718f338d0111eea312bbaf541
Kuntal Sarkar <kuntal.sarkar383@gmail.com>
2022-11-17 14:20:38 +0530

    Record To Record comparison implemented

--- xrl-core/main/default/lwc/comparingInterface/comparingInterface.js
+++ xrl-core/main/default/lwc/comparingInterface/comparingInterface.js
@@ -6,7 +6,6 @@ export default class ComparingInterface extends LightningElement {
     connectedCallback(){
         this.config.userSelections = {};
         this.config.objRecords = {};
-        this.config.showComparisonTable = true;
         libs.remoteAction(this, 'objectList', {callback: this.getAllObjects.bind(this) });
     }
     getAllObjects(cmd,data){
@@ -21,6 +20,7 @@ export default class ComparingInterface extends LightningElement {
         this.config.userSelections[selectedFor] = selectedObj;
         console.log(this.config.userSelections[selectedFor]);
         if(selectedFor.startsWith('obj')){
+            //Getting the records from depending on selected object
             libs.remoteAction(this, 'query', {
                 sObjApiName: selectedObj,
                 fields: ['Id','Name'],
@@ -34,5 +34,56 @@ export default class ComparingInterface extends LightningElement {
                 })
             });
         }
+    }
+    //handling all the events here
+    handleEvent(event){
+        let dataId = event.target.getAttribute('data-id');
+        console.log('User Click: ', dataId);
+        if(dataId === 'btn:compare'){
+            //if the both objects are same
+            if(this.config.userSelections.objOne === this.config.userSelections.objTwo){
+                this.config.showComparisonTable = true;
+                this.config.fieldsToCompare = ['Amount','ExpectedRevenue'];  //need to get this from config
+                libs.remoteAction(this, 'query', {
+                    sObjApiName: this.config.userSelections.objTwo,
+                    fields: this.config.fieldsToCompare,
+                    relField: '',
+                    addCondition:" WHERE Id='" + this.config.userSelections.recOne + "' OR Id='" + this.config.userSelections.recTwo +"'",
+                    callback: ((nodeName, data) => {
+                        console.log('length', data[nodeName].records.length);
+                        this.config.compareResult = this.compareRecords(data[nodeName].records[0],data[nodeName].records[1]);
+                        this.config.showComparisonTable = true;
+                    })
+                });
+            }
+        }
+    }
+    //This function compares 2 records
+    //accepts 2 records map and returns a single map
+    compareRecords(record1,record2){
+        let result = [];
+        for(let key in record1){
+            if(key === 'Id') continue;
+            let r = {};
+            if(record2[key]){
+                if(record1[key] === record2[key]){
+                    console.log('SAME');
+                }else if(record1[key] > record2[key]){
+                    r.css1 = 'background-color: #00ffff;';
+                    r.css2 = 'background-color: red;';
+                    console.log('NO');
+                }else{
+                    r.css2 = 'background-color: #00ffff;';
+                    r.css1 = 'background-color: red;';
+                    console.log('NO');
+                }
+                r.fieldName1 = key;
+                r.value1 = record1[key];
+                r.fieldName2 = key;
+                r.value2 = record2[key];
+                result.push(r);
+            }
+        }
+        return result;
     }
 }
