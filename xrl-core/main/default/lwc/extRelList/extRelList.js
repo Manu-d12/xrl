b76e3fb2e2a45f2f4002b2ec5e5194bef988514f
Kuntal Sarkar <kuntal.sarkar383@gmail.com>
2023-09-13 14:40:30 +0530

    HYPER-645 || action issue fix

--- xrl-core/main/default/lwc/extRelList/extRelList.js
+++ xrl-core/main/default/lwc/extRelList/extRelList.js
@@ -1198,9 +1198,9 @@ export default class extRelList extends NavigationMixin(LightningElement) {
 					field[param] = value;
 				}
 			}
-			const expandAction0 = this.config.dialog.listViewConfig.actions.find((el) => el.actionId === 'std:expand_view');
-			this.config._expandIcon = expandAction0.actionIconName;
-			this.config._expandTip = expandAction0.actionTip;
+			// const expandAction0 = this.config.dialog.listViewConfig.actions.find((el) => el.actionId === 'std:expand_view');
+			// this.config._expandIcon = expandAction0.actionIconName;
+			// this.config._expandTip = expandAction0.actionTip;
 		}
 		if (val === 'dialog:setFieldParam') {
 			let param = event.target.getAttribute('data-param');
@@ -1566,7 +1566,7 @@ export default class extRelList extends NavigationMixin(LightningElement) {
 					window.close();
 					history.back();
 				}else{
-					let url = '/lightning/n/XRL__EXRL?c__apiName='+ btoa(this.apiName) + '&c__name='+ btoa(this.name);
+					let url = '/lightning/n/TTNAMESPACE__EXRL?c__apiName='+ btoa(this.apiName) + '&c__name='+ btoa(this.name);
 					if(this.recordId !== undefined && this.recordId !== null){
 						url += '&c__recordId='+btoa(this.recordId);
 					}
