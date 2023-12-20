2bb9c36fe9e6ede076489f8f5a249df4f2e26386
dmitrystratavar <dmitry.rivlin@stratavar.com>
2023-12-20 12:59:57 +0200

    fix layouts, quick Action

--- newVersion.bat
+++ newVersion.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ECHO Creating new version
-sfdx package:version:create -p 0Ho8d000000GmmCCAS --wait 20 --code-coverage --installation-key-bypass > newVersion.log
+sfdx package:version:create -p 0Ho8d000000GmmCCAS --wait 25 --code-coverage --installation-key-bypass > newVersion.log
 ECHO ERRORLEVEL
 IF ERRORLEVEL 1 GOTO ERROR
 
--- xrl-core/main/default/lwc/quickAction/quickAction.js
+++ xrl-core/main/default/lwc/quickAction/quickAction.js
@@ -6,7 +6,7 @@ import { CloseActionScreenEvent } from 'lightning/actions';
 export default class customAction extends LightningElement {
 
 
-    actionName = location.pathname.replace(/\/.*\/(.*)$/, "$1").replace(/\.(.*__)/, '.');
+    actionName = location.pathname.replace(/\/.*\/(.*)$/, "$1").replaceAll(/__c\./ig,'.').replaceAll(/__/ig,'');
     @track config = {}
     urlParams = this.parseUrlParams();
     @track result = '';
@@ -16,45 +16,49 @@ export default class customAction extends LightningElement {
     connectedCallback() {
 
         console.log('Custom LWC', this.actionName, this.modal, this.template)//code
-        libs.remoteAction(this, 'customSoql', {
-            SOQL: "SELECT JSON__c FROM config__mdt WHERE DeveloperName = '" + this.actionName.replace('.', '_') + "'",
+
+        
+        libs.remoteAction(this, 'getMetaConfigByName', {
+            cfgName: this.actionName.replace('.', '_'),
             callback: ((nodeName, data) => {
-                this.config = JSON.parse(data[nodeName].records[0].TTNAMESPACE__JSON__c);
-
-
-
-                //Need to get a name of related list
-                /*let SOQL = "SELECT Id, (SELECT Id FROM " + this.config.orchestrator.childObjApiName + ") FROM " + this.actionName.replace(/^(.*?)\..*?$/, "$1") + " WHERE Id='" + this.urlParams.recordId + "'";
-
-                console.log(this.config, SOQL, this.urlParams);
-                libs.remoteAction(this, 'customSoql', {
-                    SOQL: SOQL,
-                    callback: ((nodeName, data) => {
-                        console.log('List of child Ids', data[nodeName]);
-                        let relatedRecords = data[nodeName].records[0][this.config.orchestrator.childObjApiName];
-                        relatedRecords.length = this.config.orchestrator?.limits?.chunkSize ? this.config.orchestrator?.limits?.chunkSize : 200;
-                        libs.remoteAction(this, 'orchestrator', {
-                            isDebug: false,
-                            operation: this.actionName.replace('.', '_'),
-                            orchestratorRequest: {
-                                rootRecordId: this.urlParams.recordId,
-                                relatedRecordIds: Array.from(relatedRecords, function (entry) { return entry.Id; }),
-                                isFirstChunk: true,
-                                isLastChunk: true
-                            },
-                            callback: ((nodeName, data) => {
-                                console.log(nodeName, data);
-                                this.result = data[nodeName];
-
-                            })
-                        })
-                    })
-                })*/
+                this.config = JSON.parse(data[nodeName]);
+                if (this.config.UI !=undefined) {
+                    this.getRecordsAndSend();                
+                }
             })
         });
 
 
     }
+
+    getRecordsAndSend() {
+        //Need to get a name of related list
+        let SOQL = "SELECT Id, (SELECT Id FROM " + this.config.orchestrator.childObjApiName + ") FROM " + this.actionName.replace(/^(.*?)\..*?$/, "$1") + " WHERE Id='" + this.urlParams.recordId + "'";
+
+        console.log(this.config, SOQL, this.urlParams);
+        libs.remoteAction(this, 'customSoql', {
+            SOQL: SOQL,
+            callback: ((nodeName, data) => {
+                console.log('List of child Ids', data[nodeName]);
+                let relatedRecords = data[nodeName].records[0][this.config.orchestrator.childObjApiName];
+                relatedRecords.length = this.config.orchestrator?.limits?.chunkSize ? this.config.orchestrator?.limits?.chunkSize : 200;
+                libs.remoteAction(this, 'orchestrator', {
+                    isDebug: false,
+                    operation: this.actionName.replace('.', '_'),
+                    orchestratorRequest: {
+                        rootRecordId: this.urlParams.recordId,
+                        relatedRecordIds: Array.from(relatedRecords, function (entry) { return entry.Id; })
+                    },
+                    callback: ((nodeName, data) => {
+                        console.log(nodeName, data);
+                        this.result = data[nodeName];
+                    })
+                })
+            })
+        })
+    }
+
+
     @api handleEvent(){
         this.dispatchEvent(new CloseActionScreenEvent());
     }
